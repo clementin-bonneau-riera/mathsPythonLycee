383951aa436f3d3e69776a5cf81bbf308ddccfc7
Clémentin <79161093+clementin-bonneau-riera@users.noreply.github.com>
2021-09-18 23:11:26 +0200

    fix bug (time exec useless)

--- fonctions/fonctionPuissancesDe2.py
+++ fonctions/fonctionPuissancesDe2.py
@@ -3,7 +3,7 @@ print("Puissances de 2.")
 
 start_number = int(input("Entrez une puissance de départ: "))
 repeat = int(input("Jusqu'à quelle puissance voulez vous allez: "))
-times_exec = 0
+times_exec = start_number
 
 while times_exec <= repeat:
     start_number = pow(2, times_exec)
