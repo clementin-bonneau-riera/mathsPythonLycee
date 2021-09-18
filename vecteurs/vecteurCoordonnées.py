73e69c42e01739c51a64e657920004f258497202
Clémentin <79161093+clementin-bonneau-riera@users.noreply.github.com>
2021-09-18 13:17:07 +0200

    add script vector coordonates

---
+++ vecteurs/vecteurCoordonnées.py
@@ -0,0 +1,12 @@
+print("Hello World")
+
+x_a = int(input("Coordonnées X de A: "))
+y_a = int(input("Coordonnées Y de A: "))
+x_b = int(input("Coordonnées X de B: "))
+y_b = int(input("Coordonnées Y de B: "))
+
+coord_X = x_b - x_a
+coord_Y = y_b - y_a
+
+print(str(coord_X))
+print(str(coord_Y))
