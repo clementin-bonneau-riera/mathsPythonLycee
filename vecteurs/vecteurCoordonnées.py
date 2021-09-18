a33dfeea86cc33652db89590cd2d21c822f13cb7
Clémentin <79161093+clementin-bonneau-riera@users.noreply.github.com>
2021-09-18 13:31:01 +0200

    minor changes

--- vecteurs/vecteurCoordonnées.py
+++ vecteurs/vecteurCoordonnées.py
@@ -1,12 +1,14 @@
 print("Hello World")
 
-x_a = int(input("Coordonnées X de A: "))
-y_a = int(input("Coordonnées Y de A: "))
-x_b = int(input("Coordonnées X de B: "))
-y_b = int(input("Coordonnées Y de B: "))
+x_a = int(input("Coordonnées x de A: "))
+y_a = int(input("Coordonnées y de A: "))
+x_b = int(input("Coordonnées x de B: "))
+y_b = int(input("Coordonnées y de B: "))
 
 coord_X = x_b - x_a
 coord_Y = y_b - y_a
 
-print(str(coord_X))
-print(str(coord_Y))
+print("")
+print("coordonnées du vecteur AB: ")
+print(str(coord_X) + "x")
+print(str(coord_Y) + "y")
