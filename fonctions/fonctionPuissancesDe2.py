416aa9f7da147895d10b8c716d56655c43eba9c9
Clémentin <79161093+clementin-bonneau-riera@users.noreply.github.com>
2021-09-18 18:58:53 +0200

    minor changes

--- fonctions/fonctionPuissancesDe2.py
+++ fonctions/fonctionPuissancesDe2.py
@@ -5,10 +5,7 @@ start_number = int(input("Entrez une puissance de départ: "))
 repeat = int(input("Jusqu'à quelle puissance voulez vous allez: "))
 times_exec = 0
 
-print(start_number)
-
 while times_exec <= repeat:
     start_number = pow(2, times_exec)
     print("2^", times_exec, ": ", start_number)
     times_exec += 1
-    
