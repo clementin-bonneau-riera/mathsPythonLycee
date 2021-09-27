37da9f9fec26ea40c1572ec2ba067bff10f90c09
Clémentin <79161093+clementin-bonneau-riera@users.noreply.github.com>
2021-09-27 18:11:21 +0200

    bug fixes: logique math + change symboles (∆ et ∅)

--- polynomeSecondDegré/polynome2ndDegEquation.py
+++ polynomeSecondDegré/polynome2ndDegEquation.py
@@ -1,16 +1,16 @@
 import math
 
 def polynome():
-    a = int(input("a: "))
-    b = int(input("b: "))
-    c = int(input("c: "))
+    a = int(input("a = "))
+    b = int(input("b = "))
+    c = int(input("c = "))
     delta = b**2 - 4 * a * c
-    print("Delta:", delta)
+    print("∆ =", delta)
     if delta < 0:
-        print("Il n'y a pas de solutions réélles à cette équation.")
+        print("S= ∅")
     else:
         if delta == 0:
-            x0 = -b / 2 * a
+            x0 = -b / (2 * a)
             print("S={", x0, "}")
         else:
             if delta > 0:
